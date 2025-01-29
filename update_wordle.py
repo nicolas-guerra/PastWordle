6fc3fd76cf54dfa06cd924acab05c5505860bc56
Nicolas Guerra <52302560+nicolas-guerra@users.noreply.github.com>
2025-01-29 12:08:52 -0500

    Add files via upload

---
+++ update_wordle.py
@@ -0,0 +1,14 @@
+from datetime import date
+from datetime import timedelta
+import urllib.request, json 
+import csv
+
+# Yesterday's date
+yesterday = date.today() - timedelta(days = 1)
+
+# Append Yesterday's Answer
+with urllib.request.urlopen(f"https://www.nytimes.com/svc/wordle/v2/{yesterday.year}-{yesterday:%m}-{yesterday:%d}.json") as url:
+        data = json.load(url)
+        with open('wordle_answers.csv','a') as f:
+            writer = csv.writer(f)
+            writer.writerow([data['days_since_launch'],data['id'],data['solution'],f"{yesterday.month}/{yesterday.day}/{yesterday.year}"])
